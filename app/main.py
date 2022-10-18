d376b78f64378a8fddb89c6b409f72c529707faf
qmqasim99 <Qmqasim99@gmail.com>
2022-10-18 13:01:09 +0100

    Add Sublist3r as code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.worker.modules.sublister import sublister_wrapper
 
 app = FastAPI()
 
@@ -7,8 +8,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/scan/")
+async def get_scan(url: str):
+    result = sublister_wrapper(url)
+    return result
+
 @app.get("/scan/{scan_id}")
 async def get_scan(scan_id: int):
     return {"scan_id": scan_id}
+
 
     
---
+++ app/worker/modules/sublister.py
@@ -0,0 +1,21 @@
+import json
+from os import remove
+from os.path import exists
+from app.worker.modules.Sublist3r import sublist3r
+# import sublist3r 
+
+def run_sublister(url:str):
+  subdomains = sublist3r.main(url, 40, url, ports= None, silent=False, verbose= True, enable_bruteforce= False, engines=None)
+  return subdomains
+
+
+def sublister_wrapper(url: str):
+    try:
+        print('sublister is running for {}'.format(url))
+        result = run_sublister(url)
+        if result:
+            print('RESULTS', result)
+        return result
+    except:
+        print('sublister failed for {}.'.format(url))
+
